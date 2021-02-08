7e663a396261e1a9055f03d1b0bf4be561e9d892
BS <bsimpson05@gmail.com>
2021-02-08 19:58:15 +1000

    Add new constructor to User Service to use DbContext.

--- Api.Business/User/UserService.cs
+++ Api.Business/User/UserService.cs
@@ -1,6 +1,7 @@
 ﻿using Api.Business.Interfaces;
 using Api.Business.User.Handlers;
 using Api.Core.Contracts.Responses;
+using Infrastructure.Database;
 using System;
 using System.Threading.Tasks;
 
@@ -8,13 +9,38 @@ namespace Api.Business.User
 {
     public class UserService : IUserService
     {
+        private readonly DatabaseContext _db;
+
+        public UserService(DatabaseContext db)
+        {
+            _db = db;
+        }
+
         public async Task<RegisterUserResponse> Register(RegisterUserQuery request)
         {
-            // we need to save the user to a database, when we work out how to add EF Core here.
+            //todo: add entity validation...
+
+            // ???
+            var id = Guid.NewGuid();
+
+            _db.Add(new Core.Entities.User
+            {
+                CountryId = request.CountryId,
+                Created = DateTimeOffset.Now,
+                Email = request.Email,
+                EmailValidatedOn = DateTimeOffset.Now,
+                Firstname = request.Firstname,
+                Id = id,
+                Surname = request.Surname,
+                Updated = null
+            });
+
+            _db.SaveChanges();
+
             return new RegisterUserResponse
             {
                 Email = request.Email,
-                UserId = Guid.NewGuid()
+                UserId = id,
             };
         }
     }
