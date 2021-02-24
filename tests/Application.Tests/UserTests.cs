7f5ba7a0f491f64269068527f8ab6856ee57620d
Craig Lister <clister@degreed.com>
2021-02-25 08:40:52 +1000

    Unit Tests working and DB Populated.

--- tests/Application.Tests/UserTests.cs
+++ tests/Application.Tests/UserTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.User;
+using Application.User.Handlers;
 using Infrastructure.Database;
 using Infrastructure.Database.Entities;
 using Infrastructure.Providers.Clock;
@@ -15,7 +16,7 @@ namespace Application.Tests
     public class UserTests
     {
         private readonly UserService _sut;
-        private readonly Guid userId = Guid.NewGuid();
+        private readonly Guid _userId = Guid.NewGuid();
         
         public UserTests()
         {
@@ -24,41 +25,42 @@ namespace Application.Tests
                 .Options;
 
             var context = new DatabaseContext(options);
+            context.Database.EnsureCreated();
             var logger = new Mock<ILogger<UserService>>();
             var clock = new Mock<IClockProvider>();
 
 
-
-            var user = new Infrastructure.Database.Entities.User
-            {
-                Created = clock.Object.UtcNow(),
-                Firstname = "Craig",
-                Id = userId,
-                Password = "password",
-                Surname = "Lister",
-                CountryId = 0,
-            };
-
-            var userRoles = new UserRole
-            {
-                Created = clock.Object.UtcNow(),
-                Role = context.Roles.FirstOrDefault(x => x.Id == 1),
-                User = user
-            };
-
-            var userEmail = new UserEmail
-            {
-                Created = clock.Object.UtcNow(),
-                Email = "craig@listerhome.com",
-                User = user,
-                ValidatedDate = clock.Object.UtcNow().AddHours(1)
-            };
-
-            context.Users.Add(user);
-            context.UserRoles.Add(userRoles);
-            context.UserEmails.Add(userEmail);
-            
-            context.SaveChanges();
+            //
+            // var user = new Infrastructure.Database.Entities.User
+            // {
+            //     Created = clock.Object.UtcNow(),
+            //     Firstname = "Craig",
+            //     Id = _userId,
+            //     Password = "password",
+            //     Surname = "Lister",
+            //     CountryId = 0,
+            // };
+            //
+            // var userRoles = new UserRole
+            // {
+            //     Created = clock.Object.UtcNow(),
+            //     Role = context.Roles.FirstOrDefault(x => x.Id == 1),
+            //     User = user
+            // };
+            //
+            // var userEmail = new UserEmail
+            // {
+            //     Created = clock.Object.UtcNow(),
+            //     Email = "craig@listerhome.com",
+            //     User = user,
+            //     ValidatedDate = clock.Object.UtcNow().AddHours(1)
+            // };
+            //
+            // context.Users.Add(user);
+            // context.UserRoles.Add(userRoles);
+            // context.UserEmails.Add(userEmail);
+            //
+            // context.SaveChanges();
             
             _sut = new UserService(context, logger.Object, clock.Object);
         }
@@ -72,9 +74,24 @@ namespace Application.Tests
         [Fact]
         public async Task GetUserById_Should_Return_Users()
         {
+           // Setup
+            var user = new RegisterUserQuery
+            {
+                Email = "craig@listerhome.com",
+                Firstname = "Craig",
+                Password = "password",
+                Surname = "Lister",
+                CountryId = 0
+            };
+            var result = await _sut.Register(user);
+            
+            // Act
             var users = await _sut.GetUsers();
             
+            // Assert
             Assert.Single(users.Data);
+            Assert.Equal(user.Email, users.Data?.FirstOrDefault().Email.Current);
+            Assert.Equal(false, users.Data?.FirstOrDefault().Email.CurrentIsValidated);
         }
     }
 }
